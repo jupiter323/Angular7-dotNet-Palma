7a78f835bcf5baaff6301f02578e78aeb6cd1d5e
jupiter <jupiter.fierce.31@gmail.com>
2019-03-13 20:43:42 +0700

    clientes complete

--- angular/src/app/main/clientes/clientes/clientes.component.ts
+++ angular/src/app/main/clientes/clientes/clientes.component.ts
@@ -1,7 +1,7 @@
 import { Component, Injector, ViewEncapsulation, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Http } from '@angular/http';
-import { ClientesServiceProxy, ClienteDto , Generos } from '@shared/service-proxies/service-proxies';
+import { ClientesServiceProxy, ClienteDto, Generos, CreateOrEditClienteDto } from '@shared/service-proxies/service-proxies';
 import { NotifyService } from '@abp/notify/notify.service';
 import { AppComponentBase } from '@shared/common/app-component-base';
 import { TokenAuthServiceProxy } from '@shared/service-proxies/service-proxies';
@@ -15,7 +15,9 @@ import { FileDownloadService } from '@shared/utils/file-download.service';
 import { EntityTypeHistoryModalComponent } from '@app/shared/common/entityHistory/entity-type-history-modal.component';
 import * as _ from 'lodash';
 import * as moment from 'moment';
-
+import * as XLSX from 'xlsx';
+import { finalize } from 'rxjs/operators';
+import { FileImportService } from '@shared/utils/file-import.service';
 @Component({
     templateUrl: './clientes.component.html',
     encapsulation: ViewEncapsulation.None,
@@ -35,8 +37,8 @@ export class ClientesComponent extends AppComponentBase {
     nombrE_CLIENTEFilter = '';
     apellidO_CLIENTEFilter = '';
     generO_CLIENTEFilter = -1;
-    maxFECHA_CLIENTEFilter : moment.Moment;
-		minFECHA_CLIENTEFilter : moment.Moment;
+    maxFECHA_CLIENTEFilter: moment.Moment;
+    minFECHA_CLIENTEFilter: moment.Moment;
     celulaR_CLIENTEFilter = '';
     direccioN_CLIENTEFilter = '';
     departamentO_CLIENTEFilter = '';
@@ -55,7 +57,8 @@ export class ClientesComponent extends AppComponentBase {
         private _notifyService: NotifyService,
         private _tokenAuth: TokenAuthServiceProxy,
         private _activatedRoute: ActivatedRoute,
-        private _fileDownloadService: FileDownloadService
+        private _fileDownloadService: FileDownloadService,
+        private _fileImportService: FileImportService,
     ) {
         super(injector);
     }
@@ -134,7 +137,7 @@ export class ClientesComponent extends AppComponentBase {
 
     exportToExcel(): void {
         this._clientesServiceProxy.getClientesToExcel(
-        this.filterText,
+            this.filterText,
             this.iD_CLIENTEFilter,
             this.nombrE_CLIENTEFilter,
             this.apellidO_CLIENTEFilter,
@@ -148,8 +151,85 @@ export class ClientesComponent extends AppComponentBase {
             this.empresA_CLIENTEFilter,
             this.profesioN_CLIENTEFilter,
         )
-        .subscribe(result => {
-            this._fileDownloadService.downloadTempFile(result);
-         });
+            .subscribe(result => {
+                this._fileDownloadService.downloadTempFile(result);
+            });
+    }
+
+    //
+
+    @ViewChild('file') file;
+    initializing
+    saving
+    isInitAndInsert = false;
+    onFileChange(evt: any) {
+        /* wire up file reader */
+        const target: DataTransfer = <DataTransfer>(evt.target);
+        if (target.files.length !== 1) throw new Error('Cannot use multiple files');
+        if (this.isInitAndInsert) this.initEntityRecords();
+        const reader: FileReader = new FileReader();
+        reader.onload = (e: any) => {
+            /* read workbook */
+            const bstr: string = e.target.result;
+            const wb: XLSX.WorkBook = XLSX.read(bstr, { type: 'binary' });
+
+            /* grab first sheet */
+            const wsname: string = wb.SheetNames[0];
+            const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+
+            /* save data */
+            let data = (XLSX.utils.sheet_to_json(ws, { header: 1 }));
+            let sendJsonData = this._fileImportService.parseJsonFromArray(data);
+            // this._paisesServiceProxy.importcsv(sendJsonData).subscribe(result => {
+            //     console.log(result);
+            // });
+
+            sendJsonData.values.forEach((x, i) => {
+                var cliente: CreateOrEditClienteDto = new CreateOrEditClienteDto();
+                cliente.iD_CLIENTE = x[0];
+                cliente.nombrE_CLIENTE = x[1];
+                cliente.apellidO_CLIENTE = x[2];
+                cliente.generO_CLIENTE = x[3];
+                cliente.fechA_CLIENTE = this._fileImportService.excelDateValueToMoment(x[4]);
+                console.log("date   :", this._fileImportService.excelDateValueToMoment(x[4]), "   excel data  :",this._fileImportService.parseDateExcel(x[4]),this._fileImportService.excelDateValueToDate(x[4]))
+                cliente.celulaR_CLIENTE = x[5];
+                cliente.direccioN_CLIENTE = x[6];
+                cliente.departamentO_CLIENTE = x[7];
+                cliente.municipiO_CLIENTE = x[8];
+                cliente.empresA_CLIENTE = x[9];
+                cliente.profesioN_CLIENTE = x[10];
+                this.insert(cliente);
+            });
+
+
+        };
+        reader.readAsBinaryString(target.files[0]);
+    }
+    //   (filter: string | null | undefined, iD_CLIENTEFilter: string | null | undefined, nOMBRE_CLIENTEFilter: string | null | undefined, aPELLIDO_CLIENTEFilter: string | null | undefined, gENERO_CLIENTEFilter: number | null | undefined, maxFECHA_CLIENTEFilter: moment.Moment | null | undefined, minFECHA_CLIENTEFilter: moment.Moment | null | undefined, cELULAR_CLIENTEFilter: string | null | undefined, dIRECCION_CLIENTEFilter: string | null | undefined, dEPARTAMENTO_CLIENTEFilter: string | null | undefined, mUNICIPIO_CLIENTEFilter: string | null | undefined, eMPRESA_CLIENTEFilter: string | null | undefined, pROFESION_CLIENTEFilter: string | null | undefined, sorting: string | null | undefined, skipCount: number | null | undefined, maxResultCount: number | null | undefined)
+    initEntityRecords(): void {
+        this.initializing = true;
+        //   this._clientesServiceProxy.getAll("", "", "", "", "", 0, 10000)
+        //       .pipe(finalize(() => { this.initializing = false; this.reloadPage() }))
+        //       .subscribe(result => {
+        //           result.items.forEach((x) => {
+        //               this._clientesServiceProxy.delete(x.cliente.id)
+        //                   .subscribe(() => {
+        //                       // this.notify.success(this.l('SuccessfullyDeleted'));
+        //                   });
+        //           })
+        //       });
+    }
+    insert(cliente: ClienteDto): void {
+        this.saving = true;
+
+        this._clientesServiceProxy.createOrEdit(cliente)
+            .pipe(finalize(() => { this.saving = false; this.reloadPage(); }))
+            .subscribe(() => {
+                this.notify.info(this.l('SavedSuccessfully'));
+            });
+    }
+
+    addFiles() {
+        this.file.nativeElement.click();
     }
 }
--- angular/src/shared/utils/file-import.service.ts
+++ angular/src/shared/utils/file-import.service.ts
@@ -1,20 +1,36 @@
 import { Injectable } from '@angular/core';
 import { AppConsts } from '@shared/AppConsts';
 import { FileDto } from '@shared/service-proxies/service-proxies';
-
+import * as moment from 'moment';
 @Injectable()
 export class FileImportService {
 
- 
+
 
     parseJsonFromArray(data: any) {
-  
+
         let jsonData = { fields: data[0], values: [] }
         data.forEach((x, i) => {
             if (i != 0) jsonData.values.push(x);
         });
         return jsonData;
     }
-  
-  
+    excelDateValueToMoment(excelData: any) {
+        return moment(this.parseDateExcel(excelData));
+
+    }
+    excelDateValueToDate(excelData: any) {
+        return new Date((excelData - (25567 + 2)) * 86400 * 1000)
+    }
+
+    parseDateExcel(excelTimestamp: any) {
+        const secondsInDay = 24 * 60 * 60;
+        const excelEpoch = new Date(1899, 11, 31);
+        const excelEpochAsUnixTimestamp = excelEpoch.getTime();
+        const missingLeapYearDay = secondsInDay * 1000;
+        const delta = excelEpochAsUnixTimestamp - missingLeapYearDay;
+        const excelTimestampAsUnixTimestamp = excelTimestamp * secondsInDay * 1000;
+        const parsed = excelTimestampAsUnixTimestamp + delta;
+        return isNaN(parsed) ? null : parsed;
+    }
 }
